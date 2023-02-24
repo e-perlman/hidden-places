08f878a89049fbb0234269ff9095e2c117779c55
Emma Perlman <emmajperlman@gmail.com>
2023-02-23 19:02:20 -0800

    create users controller, sessions controller, signup, login and logout routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+    def create
+        user=User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id]=user.id 
+            render json: user, status: :created
+        else
+            render json: {errors: ["Not authorized"]}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        if session[:user_id]
+            session.delete :user_id
+            head :no_content
+        else
+            render json: {errors:["Not logged in"]}, status: :unauthorized
+        end
+    end
+end
