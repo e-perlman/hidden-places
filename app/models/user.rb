31479ae35792917fd63c50cbc2f8ed6647fd1503
Emma Perlman <emmajperlman@gmail.com>
2023-02-25 15:16:55 -0800

    relationship model, user to user associations through relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+
+    has_many :followed_users, foreign_key: :follower_id, class_name: "Relationship"
+    has_many :followees, through: :followed_users
+
+    has_many :following_users, foreign_key: :followee_id, class_name: "Relationship"
+    has_many :followers, through: :following_users
+    
+    
     has_secure_password
 
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4..15 }
---
+++ app/serializers/relationship_serializer.rb
@@ -0,0 +1,3 @@
+class RelationshipSerializer < ActiveModel::Serializer
+  attributes :id
+end
