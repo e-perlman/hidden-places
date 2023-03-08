9ab6b6376c153fbf9dca323da921487aefa86724
Emma Perlman <emmajperlman@gmail.com>
2023-03-07 19:31:36 -0800

    fallback path for deployment purposes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   
-  resources :relationships
+ 
+    post "/signup", to: "users#create"
+    post "/login", to: "sessions#create"
+    get "/me", to: "users#show"
+    delete "/logout", to: "sessions#destroy"
+
+    get "/users", to: "users#index"
+    resources :relationships
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-
-  post "/signup", to: "users#create"
-  post "/login", to: "sessions#create"
-  get "/me", to: "users#show"
-  delete "/logout", to: "sessions#destroy"
-
-  get "/users", to: "users#index"
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
 end
